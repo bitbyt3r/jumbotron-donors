b23cb7123806d1b4328814193732553d241c11d0
Mark Murnane <mark@hackafe.net>
2022-01-07 13:54:20 -0500

    Initial Commit

---
+++ convert.py
@@ -0,0 +1,17 @@
+import sys
+import csv
+import json
+
+output = []
+with open(sys.argv[1], "r") as CSV:
+    reader = csv.DictReader(CSV)
+    for row in reader:
+        output.append({
+            "Status": row['Status'],
+            "Effective Donation": row['Effective Donation'],
+            "Public Name": row['Public Name'],
+            "Public Message": row['Public Message'],
+        })
+
+with open(sys.argv[1]+".json", "w") as JSON:
+    JSON.write(json.dumps(output, indent=2, sort_keys=True))
